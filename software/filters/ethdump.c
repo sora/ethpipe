34146e11240a8a71384add33eeb8c4555fb4ef53
sora <sora@haeena.net>
2013-01-22 23:25:29 +0900

    refoctering ethdump.c. changed output from file to stdout

--- software/filters/ethdump.c
+++ software/filters/ethdump.c
@@ -5,11 +5,14 @@
 #include <unistd.h>
 #include <signal.h>
 
-int fd;
 void set_signal (int sig);
 void sig_handler (int sig);
 
+int caught_signal = 0;
+
 int main() {
+	set_signal(SIGINT);
+
 	unsigned char SHB[] = { 0x0a, 0x0d, 0x0d, 0x0a,                           /* Section Header Block */
 				0x1c, 0x00, 0x00, 0x00,                           /* Block Total Length */
 				0x4d, 0x3c, 0x2b, 0x1a,                           /* Byte-Order Magic */
@@ -38,51 +41,42 @@ int main() {
 	long clock, pktlen;
 	int len, pb_len, i, padding;
 	unsigned char pb_head[40], pb_tail[4], padding_bit = 0x00;
-//	unsigned char pb_hdr[27];
 
-	char *filename = "ethpipe.pcapng";
-
-	fd = open( filename, O_WRONLY | O_CREAT );
-	if (fd < 0) {
-		fprintf( stderr, "Cannot open file\n" );
-		exit(1);
-	}
-	set_signal(SIGINT);
-
-	// write pcap headers (SHB+IDB)
+	// SHB
 	memcpy(obuf, SHB, sizeof(SHB));
 	offset += sizeof(SHB);
+	// IDB
 	memcpy(obuf+offset, IDB, sizeof(IDB));
 	offset += sizeof(IDB);
-	write(fd, obuf, offset);
 
 	// PB
 	while ( ( len = read(0, ibuf, 2000) >= 0 ) ) {
-		memset(obuf, 0, sizeof(obuf));
-		offset = 0;
-
 		clock  = *(long *)&ibuf[2];
 		pktlen = *(short *)&ibuf[6];
 		
 		padding = 4 - pktlen % 4;
 		if (padding == 4)
 			padding = 0;
+
 		pb_len = 28 + pktlen + padding + 4;
+
 		printf( "%08X %4d %d %d %02X%02X%02X%02X%02X%02X %02X%02X%02X%02X%02X%02X\n",
-		        clock, pktlen, padding, pb_len, 
-		        ibuf[8], ibuf[9], ibuf[10], ibuf[11], ibuf[12], ibuf[13],
-		        ibuf[14], ibuf[15], ibuf[16], ibuf[17], ibuf[18], ibuf[19] );
+			clock, pktlen, padding, pb_len,
+			ibuf[ 8], ibuf[ 9], ibuf[10], ibuf[11], ibuf[12], ibuf[13],
+			ibuf[14], ibuf[15], ibuf[16], ibuf[17], ibuf[18], ibuf[19] );
 
 		// PB header
 		sprintf( pb_head, "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
-		           0x02, 0x00, 0x00, 0x00,
-		           pb_len & 0xFF, (pb_len >> 8) & 0xFF, (pb_len >> 16) & 0xFF, pb_len >> 24,
-		           0x00, 0x00, 0x00, 0x00,
-//		           sec & 0xFF, (sec >> 8) & 0xFF, (sec >> 16) & 0xFF, sec >> 24,
-		           0x00, 0x00, 0x00, 0x00,
-		           clock & 0xFF, (clock >> 8) & 0xFF, (clock >> 16) & 0xFF, clock >> 24,
-		           pktlen & 0xFF, (pktlen >> 8) & 0xFF, (pktlen >> 16) & 0xFF, pktlen >> 24,
-		           pktlen & 0xFF, (pktlen >> 8) & 0xFF, (pktlen >> 16) & 0xFF, pktlen >> 24 );
+			         0x02,                 0x00,                  0x00,         0x00,
+			pb_len & 0xFF, (pb_len >> 8) & 0xFF, (pb_len >> 16) & 0xFF, pb_len >> 24,
+			         0x00,                 0x00,                  0x00,         0x00,
+/*
+			sec & 0xFF, (sec >> 8) & 0xFF, (sec >> 16) & 0xFF, sec >> 24,
+*/
+			         0x00,                 0x00,                  0x00,         0x00,
+			 clock & 0xFF,  (clock >> 8) & 0xFF,  (clock >> 16) & 0xFF,  clock >> 24,
+			pktlen & 0xFF, (pktlen >> 8) & 0xFF, (pktlen >> 16) & 0xFF, pktlen >> 24,
+			pktlen & 0xFF, (pktlen >> 8) & 0xFF, (pktlen >> 16) & 0xFF, pktlen >> 24 );
 		memcpy(obuf+offset, pb_head, 28);
 		offset += 28;
 
@@ -90,21 +84,25 @@ int main() {
 		memcpy(obuf+offset, ibuf+8, pktlen);
 		offset += pktlen;
 
-		// padding
-		for (i=0; i<padding; i++) {
+		// packet data padding
+		for (i=0; i<padding; i++)
 			memcpy(obuf+offset+i, &padding_bit, 1);
-		}
 		offset += padding;
 
 		// Block Total Length
 		sprintf (pb_tail, "%c%c%c%c", pb_len & 0xFF, (pb_len >> 8) & 0xFF, (pb_len >> 16) & 0xFF, pb_len >> 24 );
 		memcpy(obuf+offset, pb_tail, 4);
+		offset += 4;
 
-		// write PB
-		write(fd, obuf, pb_len);
+		// save data
+		write(1, obuf, offset);
+
+		if (caught_signal)
+			exit(0);
+
+		// flush buffer
+		offset = 0;
 	}
-
-	close(fd);
 	return 0;
 }
 
@@ -116,10 +114,7 @@ void set_signal (int sig) {
 }
 
 void sig_handler (int sig) {
-	if (sig == SIGINT) {
-		printf( "saving pcapng file..\n" );
-		close(fd);
-		exit(0);
-	}
+	if (sig == SIGINT)
+		caught_signal = 1;
 }
 

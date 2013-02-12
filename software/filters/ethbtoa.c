7db5907c4ef22731eed2a7c94be85283621da324
sora <sora@haeena.net>
2013-02-13 01:36:03 +0900

    ethbtoa.c: support 64 bit timestamp

--- software/filters/ethbtoa.c
+++ software/filters/ethbtoa.c
@@ -4,20 +4,22 @@
 
 int main()
 {
-	unsigned char ibuf[2000], obuf[6000];;
+	unsigned char ibuf[2000], obuf[6000];
 	int len, i;
-	long clock, pktlen, ftype;
-	long long dmac, smac;
+	long clock, pktlen;
 	int olen;
 
 	while ( ( len = read(0, ibuf, 2000) >= 0 ) ) {
-		clock = *(long *)&ibuf[2];
-		pktlen = *(short *)&ibuf[6];
-		ftype = *(short *)&ibuf[20];
-		sprintf( obuf, "%08X %4d %02X%02X%02X%02X%02X%02X %02X%02X%02X%02X%02X%02X %02X%02X", clock, pktlen, ibuf[8], ibuf[9], ibuf[10], ibuf[11], ibuf[12], ibuf[13], ibuf[14], ibuf[15], ibuf[16], ibuf[17], ibuf[18], ibuf[19], ibuf[20], ibuf[21]);
+		clock = *(long long *)&ibuf[0x02];
+		pktlen = *(short *)&ibuf[0x0a];
+		sprintf( obuf, "%16X %4d %02X%02X%02X%02X%02X%02X %02X%02X%02X%02X%02X%02X %02X%02X",
+			clock, pktlen,
+			ibuf[0x0c], ibuf[0x0d], ibuf[0x0e], ibuf[0x0f], ibuf[0x10], ibuf[0x11], // dst mac address
+			ibuf[0x12], ibuf[0x13], ibuf[0x14], ibuf[0x15], ibuf[0x16], ibuf[0x17], // src mac address
+			ibuf[0x18], ibuf[0x19] );                                               // frame type
 		olen = strlen(obuf);
 		for ( i=14; i<pktlen; ++i) {
-			sprintf(obuf+olen+(i-14)*3, " %02X", ibuf[i+8] );
+			sprintf(obuf+olen+(i-14)*3, " %02X", ibuf[i+0x0c] );
 		}
 		strcat(obuf, "\n" );
 		write (1, obuf, strlen(obuf));

ac8f85b94cf522c4dcb704137defac90eb9fcb99
sora <sora@haeena.net>
2013-03-17 19:27:12 +0900

    initial ethpipe core

--- cores/ethpipe/rtl/ethpipe.v
+++ cores/ethpipe/rtl/ethpipe.v
@@ -1,48 +1,29 @@
-`timescale 1ns / 1ps
+`default_nettype none
+
 `include "../rtl/setup.v"
+
 //`define DEBUG
 
 module ethpipe (
-  input         sys_rst,
-  input         sys_clk,
-  input         pci_clk,
+    input wire        sys_rst
+  , input wire        sys_clk
+  , input wire        pci_clk
 
-  // GMII interfaces for 4 MACs
-  input         gmii_0_tx_clk,
-  output [7:0]  gmii_0_txd,
-  output        gmii_0_tx_en,
-  input  [7:0]  gmii_0_rxd,
-  input         gmii_0_rx_dv,
-  input         gmii_0_rx_clk,
-
-  input         gmii_1_tx_clk,
-  output [7:0]  gmii_1_txd,
-  output        gmii_1_tx_en,
-  input  [7:0]  gmii_1_rxd,
-  input         gmii_1_rx_dv,
-  input         gmii_1_rx_clk,
-
-  input         gmii_2_tx_clk,
-  output [7:0]  gmii_2_txd,
-  output        gmii_2_tx_en,
-  input  [7:0]  gmii_2_rxd,
-  input         gmii_2_rx_dv,
-  input         gmii_2_rx_clk,
-
-  input         gmii_3_tx_clk,
-  output [7:0]  gmii_3_txd,
-  output        gmii_3_tx_en,
-  input  [7:0]  gmii_3_rxd,
-  input         gmii_3_rx_dv,
-  input         gmii_3_rx_clk
+  , // GMII interfaces
+  , input  wire       gmii_tx_clk
+  , output wire [7:0] gmii_txd
+  , output wire       gmii_tx_en
+  , input  wire [7:0] gmii_rxd
+  , input  wire       gmii_rx_dv
+  , input  wire       gmii_rx_clk
 
   // PCI user registers
 
 );
 
-assign gmii_0_txd_en = 1'b0;
-assign gmii_1_txd_en = 1'b0;
-assign gmii_2_txd_en = 1'b0;
-assign gmii_3_txd_en = 1'b0;
+assign gmii_txd_en = 1'b0;
 
 endmodule
+
+`default_nettype wire
+

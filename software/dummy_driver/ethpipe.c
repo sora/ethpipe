88e4be842ac275abcab3671d1a18caf2864122b4
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2013-04-01 21:15:30 +0900

    test_script: forwarding throughput test

--- software/dummy_driver/ethpipe.c
+++ software/dummy_driver/ethpipe.c
@@ -22,9 +22,8 @@
 #endif
 
 
-static unsigned char *mmio_ptr = 0L;
-static unsigned long mmio_start, mmio_end, mmio_flags, mmio_len;
-static char temp_buf[MAX_TEMP_BUF];
+static unsigned char *phy0_rx_ptr = 0L;
+static unsigned char *phy0_tx_ptr = 0L;
 
 static int ethpipe_open(struct inode *inode, struct file *filp)
 {
@@ -36,19 +35,19 @@ static int ethpipe_open(struct inode *inode, struct file *filp)
 static ssize_t ethpipe_read(struct file *filp, char __user *buf,
 			   size_t count, loff_t *ppos)
 {
-	int copy_len, i;
+	int copy_len;
 	unsigned int frame_len;
 
 #ifdef DEBUG
 	printk("%s\n", __func__);
 #endif
 
-	if (copy_len > (MAX_TEMP_BUF - 4096))
-		copy_len = (MAX_TEMP_BUF - 4096);
+	if (count > (MAX_TEMP_BUF - 169))
+		count = (MAX_TEMP_BUF - 169);
 
 	copy_len = (count / 169) * 169;
 
-	if ( copy_to_user( buf, mmio_ptr, copy_len ) ) {
+	if ( copy_to_user( buf, phy0_rx_ptr, copy_len ) ) {
 		printk( KERN_INFO "copy_to_user failed\n" );
 		return -EFAULT;
 	}
@@ -61,13 +60,16 @@ static ssize_t ethpipe_write(struct file *filp, const char __user *buf,
 
 {
 	int copy_len;
-	if ( count > 256 )
-		copy_len = 256;
-	else
-		copy_len = count;
-	printk("%s\n", __func__);
+	if (count > (MAX_TEMP_BUF - 169))
+		count = (MAX_TEMP_BUF - 169);
 
-	if ( copy_from_user( mmio_ptr, buf, copy_len ) ) {
+	copy_len = count;
+
+#ifdef DEBUG
+	printk("%s\n", __func__);
+#endif
+
+	if ( copy_from_user( phy0_tx_ptr, buf, copy_len ) ) {
 		printk( KERN_INFO "copy_from_user failed\n" );
 		return -EFAULT;
 	}
@@ -126,24 +128,29 @@ static int __init ethpipe_init(void)
 		return ret;
 	}
 
-	if ( ( mmio_ptr = kmalloc(MAX_TEMP_BUF, GFP_DMA) ) == 0 ) {
+	if ( ( phy0_rx_ptr = kmalloc(MAX_TEMP_BUF, GFP_DMA) ) == 0 ) {
+		printk("fail to kmalloc\n");
+		return -1;
+	}
+
+	if ( ( phy0_tx_ptr = kmalloc(MAX_TEMP_BUF, GFP_DMA) ) == 0 ) {
 		printk("fail to kmalloc\n");
 		return -1;
 	}
 
 	num = 0;
 	for ( i = 0; i < (MAX_TEMP_BUF-4096); ) {
-		memcpy( mmio_ptr+i, "FFFFFFFFFFFF 0022CF63967B 8899 23 1E 0A CF E5 3A DF 00 22 CF 63 96 7B 00 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20\n", 169);
+		memcpy( phy0_rx_ptr+i, "FFFFFFFFFFFF 0022CF63967B 8899 23 1E 0A CF E5 3A DF 00 22 CF 63 96 7B 00 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20\n", 169);
 		switch ( num % 10 ) {
-			case 0:  memcpy ( mmio_ptr+i, "001111111111", 12); break;
-			case 1:  memcpy ( mmio_ptr+i, "002222222222", 12); break;
-			case 2:  memcpy ( mmio_ptr+i, "003333333333", 12); break;
-			case 3:  memcpy ( mmio_ptr+i, "004444444444", 12); break;
-			case 4:  memcpy ( mmio_ptr+i, "005555555555", 12); break;
-			case 5:  memcpy ( mmio_ptr+i, "006666666666", 12); break;
-			case 6:  memcpy ( mmio_ptr+i, "007777777777", 12); break;
-			case 7:  memcpy ( mmio_ptr+i, "008888888888", 12); break;
-			case 8:  memcpy ( mmio_ptr+i, "009999999999", 12); break;
+			case 0:  memcpy ( phy0_rx_ptr+i, "001111111111", 12); break;
+			case 1:  memcpy ( phy0_rx_ptr+i, "002222222222", 12); break;
+			case 2:  memcpy ( phy0_rx_ptr+i, "003333333333", 12); break;
+			case 3:  memcpy ( phy0_rx_ptr+i, "004444444444", 12); break;
+			case 4:  memcpy ( phy0_rx_ptr+i, "005555555555", 12); break;
+			case 5:  memcpy ( phy0_rx_ptr+i, "006666666666", 12); break;
+			case 6:  memcpy ( phy0_rx_ptr+i, "007777777777", 12); break;
+			case 7:  memcpy ( phy0_rx_ptr+i, "008888888888", 12); break;
+			case 8:  memcpy ( phy0_rx_ptr+i, "009999999999", 12); break;
 			default: break;
 		}
 		i += 169;
@@ -158,8 +165,11 @@ static void __exit ethpipe_cleanup(void)
 {
 	misc_deregister(&ethpipe_dev);
 
-	if (mmio_ptr)
-		kfree(mmio_ptr);
+	if (phy0_rx_ptr)
+		kfree(phy0_rx_ptr);
+
+	if (phy0_tx_ptr)
+		kfree(phy0_tx_ptr);
 
 	printk("%s\n", __func__);
 }

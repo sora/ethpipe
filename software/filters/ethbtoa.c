bb206b47e4e2e9edecbb1c284e30b32a41f062f8
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2013-04-06 18:03:44 +0900

    ethbtoa.c: fixed new data-format

--- software/filters/ethbtoa.c
+++ software/filters/ethbtoa.c
@@ -9,17 +9,17 @@ int main()
 	long clock, pktlen;
 	int olen;
 
-	while ( ( len = read(0, ibuf, 2000) >= 0 ) ) {
+	while ( ( len = read(0, ibuf, 2000)) >= 0 ) {
 		clock = *(long long *)&ibuf[0x02];
-		pktlen = *(short *)&ibuf[0x0a];
+		pktlen = *(short *)&ibuf[0x0e];
 		sprintf( obuf, "%16X %4d %02X%02X%02X%02X%02X%02X %02X%02X%02X%02X%02X%02X %02X%02X",
 			clock, pktlen,
-			ibuf[0x0c], ibuf[0x0d], ibuf[0x0e], ibuf[0x0f], ibuf[0x10], ibuf[0x11], // dst mac address
-			ibuf[0x12], ibuf[0x13], ibuf[0x14], ibuf[0x15], ibuf[0x16], ibuf[0x17], // src mac address
-			ibuf[0x18], ibuf[0x19] );                                               // frame type
+			ibuf[0x10], ibuf[0x11], ibuf[0x12], ibuf[0x13], ibuf[0x14], ibuf[0x15], // dst mac address
+			ibuf[0x16], ibuf[0x17], ibuf[0x18], ibuf[0x19], ibuf[0x1a], ibuf[0x1b], // src mac address
+			ibuf[0x1c], ibuf[0x1d] );                                               // frame type
 		olen = strlen(obuf);
-		for ( i=14; i<pktlen; ++i) {
-			sprintf(obuf+olen+(i-14)*3, " %02X", ibuf[i+0x0c] );
+		for ( i=14; i<(pktlen); ++i) {
+			sprintf(obuf+olen+(i-14)*3, " %02X", ibuf[i+0x10] );
 		}
 		strcat(obuf, "\n" );
 		write (1, obuf, strlen(obuf));

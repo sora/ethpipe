695f369248a421b05377247ea2e594466742940e
sora <sora@haeena.net>
2013-04-09 10:38:28 +0900

    ethdump.c: supported current data format

--- software/filters/ethdump.c
+++ software/filters/ethdump.c
@@ -19,7 +19,7 @@ int main() {
 				0x01, 0x00, 0x00, 0x00,                           /* Major version and Minor Version */
 				0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,   /* Section Length */
 				0x1c, 0x00, 0x00, 0x00                            /* Block Total Length */
-				};
+			};
 
 	unsigned char IDB[] = { 0x01, 0x00, 0x00, 0x00,                           /* Block Type */
 				0x28, 0x00, 0x00, 0x00,                           /* Block Total Length */
@@ -33,12 +33,13 @@ int main() {
 				0x09, 0x00, 0x00, 0x00,                           /* Option value */
 				0x00, 0x00, 0x00, 0x00,                           /* Option Code and Option Length */
 				0x28, 0x00, 0x00, 0x00                            /* Block Total Length */
-				};
+			};
 
 	unsigned char ibuf[2000], obuf[4000];
 	int offset = 0;
 
-	long clock, pktlen;
+	long long clock;
+	long pktlen;
 	int len, pb_len, i, padding;
 	unsigned char pb_head[40], pb_tail[4], padding_bit = 0x00;
 
@@ -50,9 +51,12 @@ int main() {
 	offset += sizeof(IDB);
 
 	// PB
-	while ( ( len = read(0, ibuf, 2000) >= 0 ) ) {
-		clock  = *(long *)&ibuf[2];
-		pktlen = *(short *)&ibuf[6];
+	while ( (len = read(0, ibuf, 2000)) >= 0 ) {
+		if ( ibuf[0] != 0x55 || ibuf[1] != 0xd5 )
+			break;
+
+		pktlen = *(short *)&ibuf[0x02];
+		clock  = *(long long *)&ibuf[0x04];
 		
 		padding = 4 - pktlen % 4;
 		if (padding == 4)
@@ -60,28 +64,26 @@ int main() {
 
 		pb_len = 28 + pktlen + padding + 4;
 
-		printf( "%08X %4d %d %d %02X%02X%02X%02X%02X%02X %02X%02X%02X%02X%02X%02X\n",
+		printf( "%llX %lX %d %d %02X%02X%02X%02X%02X%02X %02X%02X%02X%02X%02X%02X %02X%02X\n",
 			clock, pktlen, padding, pb_len,
-			ibuf[ 8], ibuf[ 9], ibuf[10], ibuf[11], ibuf[12], ibuf[13],
-			ibuf[14], ibuf[15], ibuf[16], ibuf[17], ibuf[18], ibuf[19] );
+			ibuf[0x10], ibuf[0x11], ibuf[0x12], ibuf[0x13], ibuf[0x14], ibuf[0x15], // dst mac
+			ibuf[0x16], ibuf[0x17], ibuf[0x18], ibuf[0x19], ibuf[0x1a], ibuf[0x1b], // src mac
+			ibuf[0x1c], ibuf[0x1d] );                                               // frame type
 
 		// PB header
 		sprintf( pb_head, "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
-			         0x02,                 0x00,                  0x00,         0x00,
-			pb_len & 0xFF, (pb_len >> 8) & 0xFF, (pb_len >> 16) & 0xFF, pb_len >> 24,
-			         0x00,                 0x00,                  0x00,         0x00,
-/*
-			sec & 0xFF, (sec >> 8) & 0xFF, (sec >> 16) & 0xFF, sec >> 24,
-*/
-			         0x00,                 0x00,                  0x00,         0x00,
-			 clock & 0xFF,  (clock >> 8) & 0xFF,  (clock >> 16) & 0xFF,  clock >> 24,
-			pktlen & 0xFF, (pktlen >> 8) & 0xFF, (pktlen >> 16) & 0xFF, pktlen >> 24,
-			pktlen & 0xFF, (pktlen >> 8) & 0xFF, (pktlen >> 16) & 0xFF, pktlen >> 24 );
+			                0x02,                 0x00,                  0x00,                 0x00,
+			       pb_len & 0xFF, (pb_len >> 8) & 0xFF, (pb_len >> 16) & 0xFF,         pb_len >> 24,
+			                0x00,                 0x00,                  0x00,                 0x00,
+			(clock >> 32) & 0xFF, (clock >> 40) & 0xFF,  (clock >> 48) & 0xFF,          clock >> 56,
+			        clock & 0xFF,  (clock >> 8) & 0xFF,  (clock >> 16) & 0xFF, (clock >> 24) & 0xFF,
+			       pktlen & 0xFF, (pktlen >> 8) & 0xFF, (pktlen >> 16) & 0xFF,         pktlen >> 24,
+			       pktlen & 0xFF, (pktlen >> 8) & 0xFF, (pktlen >> 16) & 0xFF,         pktlen >> 24 );
 		memcpy(obuf+offset, pb_head, 28);
 		offset += 28;
 
 		// Packet data
-		memcpy(obuf+offset, ibuf+8, pktlen);
+		memcpy(obuf+offset, ibuf+0x10, pktlen);
 		offset += pktlen;
 
 		// packet data padding

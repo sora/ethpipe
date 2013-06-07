e075fe43f3c397ccc8f07754a8dd6ee1d541fcb3
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2013-06-08 01:58:48 +0900

    genpipe.c: remove kernel_sock code and fixed minor bugs

--- software/generic_driver/genpipe.c
+++ software/generic_driver/genpipe.c
@@ -34,6 +34,8 @@
 #define	PACKET_BUF_MAX	(1024*1024)
 #endif
 
+#define	MAX_IFR		20	/* Max Ethernet Interfaces */
+
 
 #if LINUX_VERSION_CODE > KERNEL_VERSION(3,8,0)
 #define	__devinit
@@ -56,22 +58,21 @@ struct _pbuf_dma {
 	unsigned char   *tx_read_ptr;		/* tx read ptr */
 } static pbuf0={0,0,0,0,0,0,0,0};
 
-struct socket *kernel_soc= NULL;
 struct ifreq ifr_backup;
+struct net_device* device = NULL; 
 
 int genpipe_pack_rcv(struct sk_buff *skb, struct net_device *dev, struct packet_type *pt)
 {
 	int i, frame_len;
 	unsigned char *p;
 
-	if ( strncmp( dev->name, IF_NAME, 5))
-		return 0;
+//	if ( strncmp( dev->name, IF_NAME, 5))
+//		return 0;
 
 	frame_len = (skb->len)*3+31;
 #ifdef DEBUG
 	printk(KERN_DEBUG "Test protocol: Packet Received with length: %u\n", skb->len+18);
 #endif
-
 
 	if ( (pbuf0.rx_write_ptr +  frame_len) > pbuf0.rx_end_ptr ) {
 		memcpy( pbuf0.rx_start_ptr, pbuf0.rx_write_ptr, (pbuf0.rx_write_ptr - pbuf0.rx_start_ptr ));
@@ -81,7 +82,7 @@ int genpipe_pack_rcv(struct sk_buff *skb, struct net_device *dev, struct packet_
 		pbuf0.rx_write_ptr = pbuf0.rx_start_ptr;
 	}
 
-	p = (unsigned char *)(skb->head) + (int)(skb->mac_header);
+	p = skb_mac_header(skb);
 	sprintf(pbuf0.rx_write_ptr, "%02X%02X%02X%02X%02X%02X %02X%02X%02X%02X%02X%02X %02X%02X",
 		p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9], p[10], p[11], p[12], p[13]); 
 
@@ -90,16 +91,6 @@ int genpipe_pack_rcv(struct sk_buff *skb, struct net_device *dev, struct packet_
 		sprintf(pbuf0.rx_write_ptr+30+i*3, " %02X", p[i] );
 	}
 	sprintf(pbuf0.rx_write_ptr+30+i*3, "\n");
-
-//	*(short *)(pbuf0.rx_write_ptr + 0) = 0xd555;		/* magic code 0x55d5 */
-//	*(short *)(pbuf0.rx_write_ptr + 2) = frame_len;		/* frame len */
-//	*(long long *)(pbuf0.rx_write_ptr + 4) = 0;		/* counter */
-//	*(long *)(pbuf0.rx_write_ptr + 0xc) = 0;		/* hash */
-
-//	memcpy(pbuf0.rx_write_ptr+0x10, skb->head + skb->mac_header, frame_len);
-//	memcpy(pbuf0.rx_write_ptr+0x10, skb->head + skb->mac_header, 14);
-//	memcpy(pbuf0.rx_write_ptr+0x1e, skb->data, skb->len);
-//	*(long *)(pbuf0.rx_write_ptr + 0xc + frame_len) = 0;	/* FCS */
 
 	pbuf0.rx_write_ptr += frame_len;
 
@@ -119,33 +110,8 @@ static ssize_t genpipe_read(struct file *filp, char __user *buf,
 			   size_t count, loff_t *ppos)
 {
 	int copy_len, available_read_len;
-//	int size;
-//	struct msghdr msg;
-//	struct iovec iov;
-//	mm_segment_t oldfs;
-
 #ifdef DEBUG
 	printk("%s\n", __func__);
-#endif
-#if 0
-	iov.iov_base = buf_ptr + (2+2+8+4);
-	iov.iov_len = PACKET_BUF_MAX;
-
-	msg.msg_control = NULL;
-	msg.msg_controllen = 0;
-	msg.msg_flags = 0;
-	msg.msg_name = 0;
-	msg.msg_namelen = 0;
-	msg.msg_iov = &iov;
-	msg.msg_iovlen = 1;
-
-	oldfs = get_fs();
-	set_fs(KERNEL_DS);
-	size = sock_recvmsg( kernel_soc, &msg, PACKET_BUF_MAX, msg.msg_flags );
-	set_fs( oldfs );
-#endif
-#ifdef DEBUG
-	printk(KERN_WARNING "sock_recvmsg=%d\n", size);
 #endif
 
 	if ( wait_event_interruptible( read_q, ( pbuf0.rx_read_ptr != pbuf0.rx_write_ptr ) ) )
@@ -226,24 +192,13 @@ genpipe_write_loop:
 		goto genpipe_write_exit;
 	}
 
-	iov.iov_base = pbuf0.tx_read_ptr + 16;
-	iov.iov_len = frame_len - 4;
-
-	msg.msg_control = NULL;
-	msg.msg_controllen = 0;
-	msg.msg_flags = 0;
-	msg.msg_name = 0;
-	msg.msg_namelen = 0;
-	msg.msg_iov = &iov;
-	msg.msg_iovlen = 1;
-
-	oldfs = get_fs();
-	set_fs(KERNEL_DS);
-	size = sock_sendmsg( kernel_soc, &msg, frame_len - 4);
-	set_fs( oldfs );
+//	size = sock_sendmsg( kernel_soc, &msg, frame_len - 4);
 //#ifdef DEBUG
-	printk(KERN_WARNING "frame_len=%d, sock_sendmsg=%d\n", frame_len, size);
+//	printk(KERN_WARNING "frame_len=%d, sock_sendmsg=%d\n", frame_len, size);
 ////#endif
+/**********************************/
+/* We should use dev_queue_xmit() */
+/**********************************/
 
 	pbuf0.tx_read_ptr += (frame_len + 16);
 
@@ -310,24 +265,24 @@ static struct packet_type genpipe_pack =
 static int __init genpipe_init(void)
 {
 	int ret;
-	struct sockaddr_ll sock_addr;
-	struct ifreq ifr;
 
 #ifdef MODULE
 	pr_info(genpipe_DRIVER_NAME "\n");
 #endif
 	printk("%s\n", __func__);
 
-	ret = misc_register(&genpipe_dev);
-	if (ret) {
-		printk("fail to misc_register (MISC_DYNAMIC_MINOR)\n");
-		return ret;
+	device = dev_get_by_name(&init_net, IF_NAME); 
+	if ( !device ) {
+		printk(KERN_WARNING "Could not find %s, err %d\n", IF_NAME, ret);
+		ret = -1;
+		goto error;
 	}
 
 	/* Set receive buffer */
 	if ( ( pbuf0.rx_start_ptr = kmalloc(PACKET_BUF_MAX, GFP_KERNEL) ) == 0 ) {
 		printk("fail to kmalloc\n");
-		return -1;
+		ret = -1;
+		goto error;
 	}
 	pbuf0.rx_end_ptr = (pbuf0.rx_start_ptr + PACKET_BUF_MAX - 1);
 	pbuf0.rx_write_ptr = pbuf0.rx_start_ptr;
@@ -336,54 +291,43 @@ static int __init genpipe_init(void)
 	/* Set transmitte buffer */
 	if ( ( pbuf0.tx_start_ptr = kmalloc(PACKET_BUF_MAX, GFP_KERNEL) ) == 0 ) {
 		printk("fail to kmalloc\n");
-		return -1;
+		ret = -1;
+		goto error;
 	}
 	pbuf0.tx_end_ptr = (pbuf0.tx_start_ptr + PACKET_BUF_MAX - 1);
 	pbuf0.tx_write_ptr = pbuf0.tx_start_ptr;
 	pbuf0.tx_read_ptr  = pbuf0.tx_start_ptr;
 
-	init_waitqueue_head( &write_q );
-	init_waitqueue_head( &read_q );
+	ret = misc_register(&genpipe_dev);
+	if (ret) {
+		printk("fail to misc_register (MISC_DYNAMIC_MINOR)\n");
+		goto error;
+	}
 
+	init_waitqueue_head( &read_q );
+	init_waitqueue_head( &write_q );
+
+	genpipe_pack.dev = device;
 	dev_add_pack(&genpipe_pack);
 
-	/* Create kernel socket */
-	ret = sock_create( PF_PACKET, SOCK_RAW, htons(ETH_P_ALL), &kernel_soc );
-	if (ret <0) {
-		printk(KERN_WARNING "Could not create a PF_PACKET Socket, err %d\n", ret);
-		return -1;
-	}
-
-	/* Set the IFF_PROMISC flag */
-	kernel_sock_ioctl( kernel_soc, SIOCSIFFLAGS, &ifr);
-	ifr_backup = ifr;
-	ifr.ifr_flags |= IFF_PROMISC;
-	kernel_sock_ioctl( kernel_soc, SIOCSIFFLAGS, &ifr);
-
-	memset(&sock_addr, 0, sizeof(sock_addr));
-	sock_addr.sll_family = PF_PACKET;
-	sock_addr.sll_protocol = htons(ETH_P_ALL);
-	sock_addr.sll_ifindex = 2;
-
-	ret = kernel_soc->ops->bind( kernel_soc, (struct sockaddr *)&sock_addr, sizeof(sock_addr));
-	if (ret < 0){
-		printk(KERN_WARNING "Error binding raw packet to device %d\n", ret);
-		return -1;
-	}
-
 	return 0;
+
+error:
+	if ( pbuf0.rx_start_ptr ) {
+		kfree( pbuf0.rx_start_ptr );
+		pbuf0.rx_start_ptr = NULL;
+	}
+
+	if ( pbuf0.tx_start_ptr ) {
+		kfree( pbuf0.tx_start_ptr );
+		pbuf0.tx_start_ptr = NULL;
+	}
+
+	return ret;
 }
 
 static void __exit genpipe_cleanup(void)
 {
-	/* Release kernel socket */
-	if (kernel_soc) {
-	/* Restore the IFF flag */
-		kernel_sock_ioctl( kernel_soc, SIOCSIFFLAGS, &ifr_backup);
-		sock_release(kernel_soc);
-		kernel_soc = NULL;
-	}
-
 	misc_deregister(&genpipe_dev);
 
 	dev_remove_pack(&genpipe_pack);

ef926e5dbf91ca12718505f2231f0833bc350ea4
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2013-07-16 17:57:52 +0900

    afifo18.v for simulation

---
+++ boards/ecp3versa/test/afifo18.v
@@ -0,0 +1,31 @@
+`default_nettype none
+module afifo18 (
+	input wire [17:0] Data,
+	input wire WrClock,
+	input wire RdClock,
+	input wire WrEn,
+	input wire RdEn,
+	input wire Reset,
+	input wire RPReset,
+	output wire [17:0] Q,
+	output wire Empty,
+	output wire Full
+);
+
+asfifo # (
+	.DATA_WIDTH(18),
+	.ADDRESS_WIDTH(13)
+) asfifo_inst (
+	.dout(Q), 
+	.empty(Empty),
+	.rd_en(RdEn),
+	.rd_clk(RdClock),        
+	.din(Data),  
+	.full(Full),
+	.wr_en(WrEn),
+	.wr_clk(WrClock),
+	.rst(Reset)
+);
+
+endmodule
+`default_nettype wire

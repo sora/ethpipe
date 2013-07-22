401b096a2f9c4d65015cd98efa38b8645ff2f331
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2013-07-23 03:58:10 +0900

    ethpipe.c: read operation works

--- software/driver/ethpipe.c
+++ software/driver/ethpipe.c
@@ -39,18 +39,36 @@ static DEFINE_PCI_DEVICE_TABLE(ethpipe_pci_tbl) = {
 MODULE_DEVICE_TABLE(pci, ethpipe_pci_tbl);
 
 static unsigned char *mmio0_ptr = 0L, *mmio1_ptr = 0L, *dma1_virt_ptr = 0L, *dma2_virt_ptr = 0L;
+static unsigned long long mmio0_start, mmio0_end, mmio0_flags, mmio0_len;
+static unsigned long long mmio1_start, mmio1_end, mmio1_flags, mmio1_len;
+
 static dma_addr_t dma1_phys_ptr = 0L, dma2_phys_ptr;
-static unsigned long mmio0_start, mmio0_end, mmio0_flags, mmio0_len;
-static unsigned long mmio1_start, mmio1_end, mmio1_flags, mmio1_len;
+static long *dma1_addr_start, *dma2_addr_start;
+static long *dma1_addr_cur, *dma2_addr_cur;
+static long dma1_addr_read, dma2_addr_read;
+
 static struct pci_dev *pcidev = NULL;
 static wait_queue_head_t write_q;
 static wait_queue_head_t read_q;
 
+/* receive and transmitte buffer */
+struct _pbuf_dma {
+	unsigned char   *rx_start_ptr;	/* rx buf start */
+	unsigned char   *rx_end_ptr;	/* rx buf end */
+	unsigned char   *rx_write_ptr;	/* rx write ptr */
+	unsigned char   *rx_read_ptr;	/* rx read ptr */
+	unsigned char   *tx_start_ptr;	/* tx buf start */
+	unsigned char   *tx_end_ptr;	/* tx buf end */
+	unsigned char   *tx_write_ptr;	/* tx write ptr */
+	unsigned char   *tx_read_ptr;	/* tx read ptr */
+} static pbuf0={0,0,0,0,0,0,0,0};
+
 
 static irqreturn_t ethpipe_interrupt(int irq, void *pdev)
 {
-	int status;
-	long dma1_addr_cur, dma2_addr_cur;
+	int i, status;
+	unsigned int frame_len;
+	static unsigned short hex[257], initialized = 0;
 
 	status = *(mmio0_ptr + 0x10);
 	// is ethpipe interrupt?
@@ -61,19 +79,65 @@ static irqreturn_t ethpipe_interrupt(int irq, void *pdev)
 	// clear interrupt flag
 	*(mmio0_ptr + 0x10) = status & 0xf7; 
 
-	dma1_addr_cur = *(long *)(mmio0_ptr + 0x24);
-	dma2_addr_cur = *(long *)(mmio0_ptr + 0x2c);
-
 //#ifdef DEBUG
 	printk("%s\n", __func__);
-	printk( "dma1_addr_cur=%x\n", (unsigned int)dma1_addr_cur );
-	printk( "dma2_addr_cur=%x\n", (unsigned int)dma2_addr_cur );
 //#endif
 
-#ifdef NO
-	wake_up_interruptible( &read_q );
-#endif
+	if ( unlikely(initialized == 0) ) {
+		for ( i = 0 ; i < 256 ; ++i ) {
+			sprintf( (unsigned char *)&hex[i], "%02X", i);
+		}
+		initialized = 1;
+	}
 
+	if ( *dma1_addr_cur != dma1_addr_read ) {
+		unsigned char *read_ptr, *p;
+
+		read_ptr = dma1_virt_ptr;
+		read_ptr += (int)(dma1_addr_read - dma1_phys_ptr);
+		
+		frame_len = *(short *)(read_ptr + 0);
+
+		printk( "frame_len=%4d\n", frame_len );
+
+		if ( (pbuf0.rx_write_ptr +  frame_len) > pbuf0.rx_end_ptr ) {
+			memcpy( pbuf0.rx_start_ptr, pbuf0.rx_read_ptr, (pbuf0.rx_write_ptr - pbuf0.rx_start_ptr ));
+			pbuf0.rx_write_ptr -= (pbuf0.rx_write_ptr - pbuf0.rx_start_ptr );
+			pbuf0.rx_read_ptr = pbuf0.rx_start_ptr;
+			if ( pbuf0.rx_read_ptr < pbuf0.rx_start_ptr )
+				pbuf0.rx_read_ptr = pbuf0.rx_start_ptr;
+		}
+
+		// L2 header
+		p = (read_ptr + 0x10);
+
+		for ( i = 0; i < 14; ++i ) {
+			*(unsigned short *)pbuf0.rx_write_ptr = hex[ p[i] ];
+			pbuf0.rx_write_ptr += 2;
+			if ( i == 5 || i== 11 || i == 13 ) {
+				*pbuf0.rx_write_ptr++ = ' ';
+			}
+		}
+
+		// L3 header
+		p = (read_ptr + 0x24);
+		for ( i = 0; i < (frame_len-14) ; ++i) {
+			*(unsigned short *)pbuf0.rx_write_ptr = hex[ p[i] ];
+			pbuf0.rx_write_ptr += 2;
+			if ( likely( i != (frame_len-14-1) ) ) {
+				*pbuf0.rx_write_ptr++ = ' ';
+			} else {
+				*pbuf0.rx_write_ptr++ = '\n';
+			}
+		}
+
+
+		dma1_addr_read = (long)*dma1_addr_cur;
+
+		wake_up_interruptible( &read_q );
+	}
+
+lend:
 	return IRQ_HANDLED;
 }
 
@@ -88,26 +152,29 @@ static int ethpipe_open(struct inode *inode, struct file *filp)
 }
 
 static ssize_t ethpipe_read(struct file *filp, char __user *buf,
-			   size_t count, loff_t *ppos)
+			size_t count, loff_t *ppos)
 {
-	int copy_len;
-
+	int copy_len, available_read_len;
 #ifdef DEBUG
 	printk("%s\n", __func__);
 #endif
 
-#ifdef	NO
 	if ( wait_event_interruptible( read_q, ( pbuf0.rx_read_ptr != pbuf0.rx_write_ptr ) ) )
 		return -ERESTARTSYS;
 
+	available_read_len = (pbuf0.rx_write_ptr - pbuf0.rx_read_ptr);
+
+	if ( count > available_read_len )
+		copy_len = available_read_len;
+	else
+		copy_len = count;
 
 	if ( copy_to_user( buf, pbuf0.rx_read_ptr, copy_len ) ) {
 		printk( KERN_INFO "copy_to_user failed\n" );
 		return -EFAULT;
 	}
-#endif
 
-	copy_len = count;
+	pbuf0.rx_read_ptr += copy_len;
 
 	return copy_len;
 }
@@ -143,12 +210,29 @@ static int ethpipe_release(struct inode *inode, struct file *filp)
 	return 0;
 }
 
-static unsigned int ethpipe_poll(struct file *filp, poll_table *wait)
+static unsigned int ethpipe_poll( struct file* filp, poll_table* wait )
 {
-	printk("%s\n", __func__);
-	return 0;
-}
+	unsigned int retmask = 0;
 
+#ifdef DEBUG
+	printk("%s\n", __func__);
+#endif
+
+	poll_wait( filp, &read_q,  wait );
+//      poll_wait( filp, &write_q, wait );
+
+	if ( pbuf0.rx_read_ptr != pbuf0.rx_write_ptr ) {
+		retmask |= ( POLLIN  | POLLRDNORM );
+//		log_format( "POLLIN  | POLLRDNORM" );
+	}
+/*
+   読み込みデバイスが EOF の場合は retmask に POLLHUP を設定
+   デバイスがエラー状態である場合は POLLERR を設定
+   out-of-band データが読み出せる場合は POLLPRI を設定
+ */
+
+	return retmask;
+}
 
 static int ethpipe_ioctl(struct inode *inode, struct file *filp,
 			unsigned int cmd, unsigned long arg)
@@ -190,11 +274,11 @@ static int __devinit ethpipe_init_one (struct pci_dev *pdev,
 
 	rc = pci_enable_device (pdev);
 	if (rc)
-		goto err_out;
+		goto error;
 
 	rc = pci_request_regions (pdev, DRV_NAME);
 	if (rc)
-		goto err_out;
+		goto error;
 
 	++board_idx;
 
@@ -215,7 +299,7 @@ static int __devinit ethpipe_init_one (struct pci_dev *pdev,
 	mmio0_ptr = ioremap(mmio0_start, mmio0_len);
 	if (!mmio0_ptr) {
 		printk(KERN_ERR "cannot ioremap MMIO0 base\n");
-		goto err_out;
+		goto error;
 	}
 
 	mmio1_start = pci_resource_start (pdev, 2);
@@ -231,24 +315,31 @@ static int __devinit ethpipe_init_one (struct pci_dev *pdev,
 	mmio1_ptr = ioremap_wc(mmio1_start, mmio1_len);
 	if (!mmio1_ptr) {
 		printk(KERN_ERR "cannot ioremap MMIO1 base\n");
-		goto err_out;
+		goto error;
 	}
 
 	dma1_virt_ptr = dma_alloc_coherent( &pdev->dev, PACKET_BUF_MAX, &dma1_phys_ptr, GFP_KERNEL);
 	if (!dma1_virt_ptr) {
 		printk(KERN_ERR "cannot dma1_alloc_coherent\n");
-		goto err_out;
+		goto error;
 	}
-	printk( KERN_INFO "dma1_virt_ptr  : %X\n", (unsigned int)dma1_virt_ptr );
-	printk( KERN_INFO "dma1_phys_ptr  : %X\n", (unsigned int)dma1_phys_ptr );
+{
+int i;
+static unsigned char *p;
+p = dma1_virt_ptr;
+for (i=0; i<(PACKET_BUF_MAX/4); ++i)
+	*p++ = i;
+}
+	printk( KERN_INFO "dma1_virt_ptr  : %llX\n", (long long)dma1_virt_ptr );
+	printk( KERN_INFO "dma1_phys_ptr  : %llX\n", (long long)dma1_phys_ptr );
 
 	dma2_virt_ptr = dma_alloc_coherent( &pdev->dev, PACKET_BUF_MAX, &dma2_phys_ptr, GFP_KERNEL);
 	if (!dma2_virt_ptr) {
 		printk(KERN_ERR "cannot dma2_alloc_coherent\n");
-		goto err_out;
+		goto error;
 	}
-	printk( KERN_INFO "dma2_virt_ptr  : %X\n", (unsigned int)dma2_virt_ptr );
-	printk( KERN_INFO "dma2_phys_ptr  : %X\n", (unsigned int)dma2_phys_ptr );
+	printk( KERN_INFO "dma2_virt_ptr  : %llX\n", (long long)dma2_virt_ptr );
+	printk( KERN_INFO "dma2_phys_ptr  : %llX\n", (long long)dma2_phys_ptr );
 
 	if (request_irq(pdev->irq, ethpipe_interrupt, IRQF_SHARED, DRV_NAME, pdev)) {
 		printk(KERN_ERR "cannot request_irq\n");
@@ -256,15 +347,48 @@ static int __devinit ethpipe_init_one (struct pci_dev *pdev,
 	
 	pcidev = pdev;
 
+	/* Set DMA Pointer */
+	dma1_addr_start = (mmio0_ptr + 0x20);
+	dma1_addr_cur   = (mmio0_ptr + 0x24);
+	dma2_addr_start = (mmio0_ptr + 0x28);
+	dma2_addr_cur   = (mmio0_ptr + 0x2c);
+
 	/* set DMA Buffer length */
 	*(long *)(mmio0_ptr + 0x14)  = PACKET_BUF_MAX;
 
 	/* set DMA1 start address */
-	*(long *)(mmio0_ptr + 0x20)  = dma1_phys_ptr;
+	*dma1_addr_start  = dma1_phys_ptr;
 
 	/* set DMA2 start address */
-	*(long *)(mmio0_ptr + 0x28)  = dma2_phys_ptr;
+	*dma2_addr_start  = dma2_phys_ptr;
 
+	/* set DMA read pointer */
+	dma1_addr_read = dma1_phys_ptr;
+	dma2_addr_read = dma2_phys_ptr;
+
+
+	/* Set receive buffer */
+	if ( ( pbuf0.rx_start_ptr = kmalloc(PACKET_BUF_MAX, GFP_KERNEL) ) == 0 ) {
+		printk("fail to kmalloc\n");
+		rc = -1;
+		goto error;
+	}
+	pbuf0.rx_end_ptr = (pbuf0.rx_start_ptr + PACKET_BUF_MAX - 1);
+	pbuf0.rx_write_ptr = pbuf0.rx_start_ptr;
+	pbuf0.rx_read_ptr  = pbuf0.rx_start_ptr;
+
+	/* Set transmitte buffer */
+	if ( ( pbuf0.tx_start_ptr = kmalloc(PACKET_BUF_MAX, GFP_KERNEL) ) == 0 ) {
+		printk("fail to kmalloc\n");
+		rc = -1;
+		goto error;
+	}
+	pbuf0.tx_end_ptr = (pbuf0.tx_start_ptr + PACKET_BUF_MAX - 1);
+	pbuf0.tx_write_ptr = pbuf0.tx_start_ptr;
+	pbuf0.tx_read_ptr  = pbuf0.tx_start_ptr;
+
+
+	/* register character device */
 	sprintf( name, "%s/%d", DRV_NAME, board_idx );
 	ethpipe_dev.name = name,
 	rc = misc_register(&ethpipe_dev);
@@ -275,9 +399,20 @@ static int __devinit ethpipe_init_one (struct pci_dev *pdev,
 
 	return 0;
 
-err_out:
+error:
+	if ( pbuf0.rx_start_ptr ) {
+		kfree( pbuf0.rx_start_ptr );
+		pbuf0.rx_start_ptr = NULL;
+	}
+
+	if ( pbuf0.tx_start_ptr ) {
+		kfree( pbuf0.tx_start_ptr );
+		pbuf0.tx_start_ptr = NULL;
+	}
+
 	pci_release_regions (pdev);
 	pci_disable_device (pdev);
+
 	return -1;
 }
 
@@ -295,6 +430,16 @@ static void __devexit ethpipe_remove_one (struct pci_dev *pdev)
 		iounmap(mmio1_ptr);
 		mmio1_ptr = 0L;
 	}
+	if ( pbuf0.rx_start_ptr ) {
+		kfree( pbuf0.rx_start_ptr );
+		pbuf0.rx_start_ptr = NULL;
+	}
+
+	if ( pbuf0.tx_start_ptr ) {
+		kfree( pbuf0.tx_start_ptr );
+		pbuf0.tx_start_ptr = NULL;
+	}
+
 	pci_release_regions (pdev);
 	pci_disable_device (pdev);
 	printk("%s\n", __func__);

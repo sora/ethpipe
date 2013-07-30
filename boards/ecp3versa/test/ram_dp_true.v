139aa6411fbb71a8367a692540a3b51fa2158060
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2013-07-30 23:25:48 +0900

    ram_dp_true.v: dual port ram for simulation

--- boards/ecp3versa/test/ram_dp_true.v
+++ boards/ecp3versa/test/ram_dp_true.v
@@ -15,8 +15,30 @@ module ram_dp_true  (
   , input  WrB
   , input  ResetA
   , input  ResetB
-  , output [15:0] QA
-  , output [15:0] QB
+  , output reg [15:0] QA
+  , output reg [15:0] QB
 );
+
+reg [15:0] mem [0:16383];
+
+always @(posedge ClockA) begin
+	if (WrA) begin
+		if (ByteEnA[0])
+			mem[AddressA][7:0] <= DataInA[7:0];
+		if (ByteEnA[1])
+			mem[AddressA][15:8] <= DataInA[15:8];
+	end
+	QA <= mem[AddressA];
+end
+
+always @(posedge ClockB) begin
+	if (WrB) begin
+		if (ByteEnB[0])
+			mem[AddressB][7:0] <= DataInB[7:0];
+		if (ByteEnB[1])
+			mem[AddressB][15:8] <= DataInB[15:8];
+	end
+	QB <= mem[AddressB];
+end
 
 endmodule

28cf3042ce1e5887a607903fc58faad5537dd161
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2013-08-14 17:35:07 +0900

    gmii2fifo18.v: fixed minor bug and cleanup

--- cores/gmii2fifo18/rtl/gmii2fifo18.v
+++ cores/gmii2fifo18/rtl/gmii2fifo18.v
@@ -1,16 +1,16 @@
 module gmii2fifo18 # (
 	parameter Gap = 4'h2
 ) (
-	input         sys_rst,
-	input [63:0]  global_counter,
-	input         gmii_rx_clk,
-	input         gmii_rx_dv,
-	input  [7:0]  gmii_rxd,
+	input sys_rst,
+	input [63:0] global_counter,
+	input gmii_rx_clk,
+	input gmii_rx_dv,
+	input [7:0] gmii_rxd,
 	// FIFO
 	output [17:0]  din,
-	input         full,
-        output reg    wr_en,
-	output        wr_clk,
+	input full,
+	output reg  wr_en,
+	output wr_clk,
 	output reg [7:0] wr_count
 );
 
@@ -38,13 +38,13 @@ reg rxc = 1'b0;
 reg [3:0] gap_count = 4'h0;
 always @(posedge gmii_rx_clk) begin
 	if (sys_rst) begin
-		gap_count <= Gap;
+		gap_count <= 4'h0;
 		rxd <= 18'h000;
 		wr_en <= 1'b0;
 		wr_count <= 8'h0;
 		state <= STATE_SFD0;
 	end else begin
-                wr_en <= 1'b0;
+		wr_en <= 1'b0;
 		if (gmii_rx_dv == 1'b1) begin
 			case (state)
 				STATE_SFD0: begin
@@ -55,34 +55,34 @@ always @(posedge gmii_rx_clk) begin
 				end
 				STATE_SFD1: begin
 					rxd[ 7:0] <= global_counter_latch[15: 8];
-               	 			wr_en <= 1'b1;
+	       	 			wr_en <= 1'b1;
 					state <= STATE_SFD2;
 				end
 				STATE_SFD2: begin
-					rxd[17:8] <= {2'b11, global_counter[23:16]};
+					rxd[17:8] <= {2'b11, global_counter_latch[23:16]};
 					state <= STATE_SFD3;
 				end
 				STATE_SFD3: begin
 					rxd[ 7:0] <= global_counter_latch[31:24];
-               	 			wr_en <= 1'b1;
+	       	 			wr_en <= 1'b1;
 					state <= STATE_SFD4;
 				end
 				STATE_SFD4: begin
-					rxd[17:8] <= {2'b11, global_counter[39:32]};
+					rxd[17:8] <= {2'b11, global_counter_latch[39:32]};
 					state <= STATE_SFD5;
 				end
 				STATE_SFD5: begin
 					rxd[ 7:0] <= global_counter_latch[47:40];
-               	 			wr_en <= 1'b1;
+	       	 			wr_en <= 1'b1;
 					state <= STATE_SFD6;
 				end
 				STATE_SFD6: begin
-					rxd[17:8] <= {2'b11, global_counter[55:48]};
+					rxd[17:8] <= {2'b11, global_counter_latch[55:48]};
 					state <= STATE_SFD7;
 				end
 				STATE_SFD7: begin
 					rxd[ 7:0] <= global_counter_latch[63:56];
-               	 			wr_en <= 1'b1;
+	       	 			wr_en <= 1'b1;
 					state <= STATE_DATAH;
 				end
 				STATE_DATAH: begin
@@ -92,7 +92,7 @@ always @(posedge gmii_rx_clk) begin
 				STATE_DATAL: begin
 					rxd[16] <= 1'b1;
 					rxd[7:0] <= gmii_rxd[7:0];
-               	 			wr_en <= 1'b1;
+	       	 			wr_en <= 1'b1;
 					state <= STATE_DATAH;
 				end
 			endcase
@@ -104,7 +104,7 @@ always @(posedge gmii_rx_clk) begin
 				rxd <= 18'h000;
 			end
 			if (gap_count != 4'h0) begin
-                		wr_en <= 1'b1;
+				wr_en <= 1'b1;
 				gap_count <= gap_count - 4'h1;
 			end
 		end

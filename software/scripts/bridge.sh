53bcecb655f999c1341616996e178c7e3e38806e
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2013-09-25 22:14:21 +0900

    bridge.sh: support all functions

--- software/scripts/bridge.sh
+++ software/scripts/bridge.sh
@@ -2,9 +2,14 @@
 # usage: bridge.sh </dev/ethpipe/0
 #
 MY_PORT="0"
-MAC_LEARNING="001122334455"
+MAC_LEARNING=""
+#MAC_LEARNING="001122334455"
 TEMP_DIR="/tmp/"
 MAC_LEARNING_FILE=$TEMP_DIR/MAC$MY_PORT.txt
+MAC0_LEARNING_FILE=$TEMP_DIR/MAC0.txt; touch $MAC0_LEARNING_FILE
+MAC1_LEARNING_FILE=$TEMP_DIR/MAC1.txt; touch $MAC1_LEARNING_FILE
+MAC2_LEARNING_FILE=$TEMP_DIR/MAC2.txt; touch $MAC2_LEARNING_FILE
+MAC3_LEARNING_FILE=$TEMP_DIR/MAC3.txt; touch $MAC3_LEARNING_FILE
 while true
 do
     read FRAME
@@ -38,5 +43,22 @@ do
            echo $MAC_LEARNING > $MAC_LEARNING_FILE
        fi
        # search port number by DMAC
+       MAC_LEARNING0=`cat $MAC0_LEARNING_FILE`
+       if [[ "$MAC0_LEARNING" =~ "$DMAC" ]]; then
+           echo $FRAME >/dev/ethpipe/0
+       fi
+       MAC_LEARNING1=`cat $MAC1_LEARNING_FILE`
+       if [[ "$MAC1_LEARNING" =~ "$DMAC" ]]; then
+           echo $FRAME >/dev/ethpipe/1
+       fi
+       MAC_LEARNING2=`cat $MAC2_LEARNING_FILE`
+       if [[ "$MAC2_LEARNING" =~ "$DMAC" ]]; then
+           echo $FRAME >/dev/ethpipe/2
+       fi
+       MAC_LEARNING3=`cat $MAC3_LEARNING_FILE`
+       if [[ "$MAC3_LEARNING" =~ "$DMAC" ]]; then
+           echo $FRAME >/dev/ethpipe/3
+       fi
     fi
+#break
 done

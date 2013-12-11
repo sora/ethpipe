985836b8682a940cef6f0fea2cf70a483902fe25
sora <sora@haeena.net>
2013-12-12 03:52:40 +0900

    driver: changed btoa function

--- software/driver/ethpipe.c
+++ software/driver/ethpipe.c
@@ -83,6 +83,40 @@ static const unsigned char _atob[] = {
 	_SKP, _SKP, _SKP, _SKP, _SKP, _SKP, _SKP, _SKP,     /* 240-247 */
 	_SKP, _SKP, _SKP, _SKP, _SKP, _SKP, _SKP, _SKP };   /* 248-255 */
 
+static const short _btoa[257] = {
+	0x3030, 0x3130, 0x3230, 0x3330, 0x3430, 0x3530, 0x3630, 0x3730,     /* 0-7 */
+	0x3830, 0x3930, 0x4130, 0x4230, 0x4330, 0x4430, 0x4530, 0x4630,     /* 8-15 */
+	0x3031, 0x3131, 0x3231, 0x3331, 0x3431, 0x3531, 0x3631, 0x3731,     /* 16-23 */
+	0x3831, 0x3931, 0x4131, 0x4231, 0x4331, 0x4431, 0x4531, 0x4631,     /* 24-31 */
+	0x3032, 0x3132, 0x3232, 0x3332, 0x3432, 0x3532, 0x3632, 0x3732,     /* 32-39 */
+	0x3832, 0x3932, 0x4132, 0x4232, 0x4332, 0x4432, 0x4532, 0x4632,     /* 40-47 */
+	0x3033, 0x3133, 0x3233, 0x3333, 0x3433, 0x3533, 0x3633, 0x3733,     /* 48-55 */
+	0x3833, 0x3933, 0x4133, 0x4233, 0x4333, 0x4433, 0x4533, 0x4633,     /* 56-63 */
+	0x3034, 0x3134, 0x3234, 0x3334, 0x3434, 0x3534, 0x3634, 0x3734,     /* 64-71 */
+	0x3834, 0x3934, 0x4134, 0x4234, 0x4334, 0x4434, 0x4534, 0x4634,     /* 72-79 */
+	0x3035, 0x3135, 0x3235, 0x3335, 0x3435, 0x3535, 0x3635, 0x3735,     /* 80-87 */
+	0x3835, 0x3935, 0x4135, 0x4235, 0x4335, 0x4435, 0x4535, 0x4635,     /* 88-95 */
+	0x3036, 0x3136, 0x3236, 0x3336, 0x3436, 0x3536, 0x3636, 0x3736,     /* 96-103 */
+	0x3836, 0x3936, 0x4136, 0x4236, 0x4336, 0x4436, 0x4536, 0x4636,     /* 104-111 */
+	0x3037, 0x3137, 0x3237, 0x3337, 0x3437, 0x3537, 0x3637, 0x3737,     /* 112-119 */
+	0x3837, 0x3937, 0x4137, 0x4237, 0x4337, 0x4437, 0x4537, 0x4637,     /* 120-127 */
+	0x3038, 0x3138, 0x3238, 0x3338, 0x3438, 0x3538, 0x3638, 0x3738,     /* 128-135 */
+	0x3838, 0x3938, 0x4138, 0x4238, 0x4338, 0x4438, 0x4538, 0x4638,     /* 136-143 */
+	0x3039, 0x3139, 0x3239, 0x3339, 0x3439, 0x3539, 0x3639, 0x3739,     /* 144-151 */
+	0x3839, 0x3939, 0x4139, 0x4239, 0x4339, 0x4439, 0x4539, 0x4639,     /* 152-159 */
+	0x3041, 0x3141, 0x3241, 0x3341, 0x3441, 0x3541, 0x3641, 0x3741,     /* 160-167 */
+	0x3841, 0x3941, 0x4141, 0x4241, 0x4341, 0x4441, 0x4541, 0x4641,     /* 168-175 */
+	0x3042, 0x3142, 0x3242, 0x3342, 0x3442, 0x3542, 0x3642, 0x3742,     /* 176-183 */
+	0x3842, 0x3942, 0x4142, 0x4242, 0x4342, 0x4442, 0x4542, 0x4642,     /* 184-191 */
+	0x3043, 0x3143, 0x3243, 0x3343, 0x3443, 0x3543, 0x3643, 0x3743,     /* 192-199 */
+	0x3843, 0x3943, 0x4143, 0x4243, 0x4343, 0x4443, 0x4543, 0x4643,     /* 200-207 */
+	0x3044, 0x3144, 0x3244, 0x3344, 0x3444, 0x3544, 0x3644, 0x3744,     /* 208-215 */
+	0x3844, 0x3944, 0x4144, 0x4244, 0x4344, 0x4444, 0x4544, 0x4644,     /* 216-223 */
+	0x3045, 0x3145, 0x3245, 0x3345, 0x3445, 0x3545, 0x3645, 0x3745,     /* 224-231 */
+	0x3845, 0x3945, 0x4145, 0x4245, 0x4345, 0x4445, 0x4545, 0x4645,     /* 232-239 */
+	0x3046, 0x3146, 0x3246, 0x3346, 0x3446, 0x3546, 0x3646, 0x3746,     /* 240-247 */
+	0x3846, 0x3946, 0x4146, 0x4246, 0x4346, 0x4446, 0x4546, 0x4646 };   /* 248-255 */
+
 
 static DEFINE_PCI_DEVICE_TABLE(ethpipe_pci_tbl) = {
 	{0x3776, 0x8001, PCI_ANY_ID, PCI_ANY_ID, 0, 0, 0 },
@@ -207,7 +241,6 @@ void  tasklet_body( unsigned long value )
 //	int status;
 	unsigned long flags;
 	unsigned short frame_len;
-	static unsigned short hex[257], initialized = 0;
 	static long long dma1_addr_write_ascii;
 	static int dma1_overflow, mem1_overflow = -1;
 	long long dma1_value;
@@ -219,12 +252,6 @@ void  tasklet_body( unsigned long value )
 
 	spin_lock_irqsave( &tasklet_spinlock, flags );
 
-	if ( unlikely(initialized == 0) ) {
-		for ( i = 0 ; i < 256 ; ++i ) {
-			sprintf( (unsigned char *)&hex[i], "%02X", i);
-		}
-		initialized = 1;
-	}
 	dma1_value = (long)*dma1_addr_cur;
 	dma1_addr_write_ascii = (dma1_value & 0x0fffffff) | (dma1_phys_ptr  & 0xf0000000);
 	dma1_overflow = dma1_value >> 28;
@@ -268,7 +295,7 @@ void  tasklet_body( unsigned long value )
 					}
 				}
 				for ( i = 0; i < 8; ++i ) {
-					*(unsigned short *)pbuf0.rx_write_ascii_ptr = hex[ i > 1 ? *p : 0 ];
+					*(unsigned short *)pbuf0.rx_write_ascii_ptr = _btoa[ i > 1 ? *p : 0 ];
 					pbuf0.rx_write_ascii_ptr += 2;
 					if ( unlikely(pbuf0.rx_write_ascii_ptr > pbuf0.rx_end_ascii_ptr) )
 						pbuf0.rx_write_ascii_ptr -= ASCII_BUF_MAX;
@@ -299,7 +326,7 @@ void  tasklet_body( unsigned long value )
 
 				// L2 header
 				for ( i = 0; i < 14; ++i ) {
-					*(unsigned short *)pbuf0.rx_write_ascii_ptr = hex[ *p ];
+					*(unsigned short *)pbuf0.rx_write_ascii_ptr = _btoa[ *p ];
 					pbuf0.rx_write_ascii_ptr += 2;
 					if ( unlikely( pbuf0.rx_write_ascii_ptr > pbuf0.rx_end_ascii_ptr ) )
 						pbuf0.rx_write_ascii_ptr -= ASCII_BUF_MAX;
@@ -324,7 +351,7 @@ void  tasklet_body( unsigned long value )
 #else
 				for ( i = 0; i < (frame_len-14) ; ++i) {
 #endif
-					*(unsigned short *)pbuf0.rx_write_ascii_ptr = hex[ *p ];
+					*(unsigned short *)pbuf0.rx_write_ascii_ptr = _btoa[ *p ];
 					pbuf0.rx_write_ascii_ptr += 2;
 					if ( unlikely(pbuf0.rx_write_ascii_ptr > pbuf0.rx_end_ascii_ptr) )
 						pbuf0.rx_write_ascii_ptr -= ASCII_BUF_MAX;
@@ -370,7 +397,7 @@ void  tasklet_body( unsigned long value )
 #ifdef USE_TIMER
 				p += 0x08;
 				for ( i = 0; i < 8; ++i ) {
-					*(unsigned short *)pbuf0.rx_write_ascii_ptr = hex[ i > 1 ? *p : 0 ];
+					*(unsigned short *)pbuf0.rx_write_ascii_ptr = _btoa[ i > 1 ? *p : 0 ];
 					pbuf0.rx_write_ascii_ptr += 2;
 					if ( unlikely( i == 7 ) ) {
 						*pbuf0.rx_write_ascii_ptr++ = ' ';
@@ -383,7 +410,7 @@ void  tasklet_body( unsigned long value )
 
 				// L2 header
 				for ( i = 0; i < 14; ++i ) {
-					*(unsigned short *)pbuf0.rx_write_ascii_ptr = hex[ *p ];
+					*(unsigned short *)pbuf0.rx_write_ascii_ptr = _btoa[ *p ];
 					pbuf0.rx_write_ascii_ptr += 2;
 					if ( unlikely( i == 5 || i== 11 || i == 13 ) ) {
 						*pbuf0.rx_write_ascii_ptr++ = ' ';
@@ -397,7 +424,7 @@ void  tasklet_body( unsigned long value )
 #else
 				for ( i = 0; i < (frame_len-14) ; ++i) {
 #endif
-					*(unsigned short *)pbuf0.rx_write_ascii_ptr = hex[ *p ];
+					*(unsigned short *)pbuf0.rx_write_ascii_ptr = _btoa[ *p ];
 					pbuf0.rx_write_ascii_ptr += 2;
 					*pbuf0.rx_write_ascii_ptr++ = ' ';
 					++p;

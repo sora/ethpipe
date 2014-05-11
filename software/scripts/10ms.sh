977afbc2551cfe8ba417a0a1187f5172af5798b3
sora <sora@haeena.net>
2014-05-11 17:36:59 +0900

    update 10ms.sh

--- software/scripts/10ms.sh
+++ software/scripts/10ms.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+# usage: ./10ms.sh < /dev/ethpipe/0 > /dev/ethpipe0
 
-delay=$(( 10000000 / 8 ))        # 10ms
+delay=$(( 10000000 / 8 ))     # 10ms
 
 while true
 do
-	read pkt
+	read pkt       # recv
 
 	if [[ $pkt =~ ^[0-9A-F]{16} ]]; then
 		recv_ts=${pkt:0:16}
 		frame=${pkt:16}
 
-		printf "%016X$frame\n" $(( 16#$recv_ts + 10#$delay )) > /dev/ethpipe/0
-#		echo $recv_ts
-#		printf "%016X$frame\n" $(( 16#$recv_ts + 10#$delay ))
+		echo "received: ts=$recv_ts" > /dev/stderr
+		printf "%016X$frame\n" $(( 16#$recv_ts + 10#$delay ))  # send
 	fi
 done
 
